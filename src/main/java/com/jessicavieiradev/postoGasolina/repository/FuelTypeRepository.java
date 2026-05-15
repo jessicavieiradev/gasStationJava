8fca800d070f2c7effc95c93ed654f3935bc7c10
“jessicavieiradev” <misspandaicebear@gmail.com>
2026-05-15 11:17:42 -0300

    feat: add new method for fuel type repository

--- src/main/java/com/jessicavieiradev/postoGasolina/repository/FuelTypeRepository.java
+++ src/main/java/com/jessicavieiradev/postoGasolina/repository/FuelTypeRepository.java
@@ -1,9 +1,12 @@
 package com.jessicavieiradev.postoGasolina.repository;
 
 import com.jessicavieiradev.postoGasolina.domain.entity.FuelType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface FuelTypeRepository extends JpaRepository<FuelType, UUID> {
+    boolean existsByName(String name);
 }
