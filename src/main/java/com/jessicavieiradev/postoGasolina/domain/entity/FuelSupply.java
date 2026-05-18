cf161e0df2b926be5f2453bcfa12a67eaf54500c
“jessicavieiradev” <misspandaicebear@gmail.com>
2026-05-18 09:18:29 -0300

    feat: implement Fuel Supply soft delete logic

--- src/main/java/com/jessicavieiradev/postoGasolina/domain/entity/FuelSupply.java
+++ src/main/java/com/jessicavieiradev/postoGasolina/domain/entity/FuelSupply.java
@@ -1,10 +1,13 @@
 package com.jessicavieiradev.postoGasolina.domain.entity;
 
+import com.jessicavieiradev.postoGasolina.domain.enums.Status;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.math.BigDecimal;
@@ -17,6 +20,8 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@SQLDelete(sql = "UPDATE fuel_supplies SET status = 'inativo' WHERE id = ?")
+@SQLRestriction("status = 'ativo'")
 public class FuelSupply {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -33,6 +38,10 @@ public class FuelSupply {
 
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status = Status.ativo;
 
     @ManyToOne
     @JoinColumn(name = "fuel_pump_id",nullable = false)
