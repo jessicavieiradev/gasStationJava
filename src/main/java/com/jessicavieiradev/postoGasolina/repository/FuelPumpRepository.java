d2d0d51d424cc980abf40d278e8d8aa9cbbdd177
“jessicavieiradev” <misspandaicebear@gmail.com>
2026-05-18 09:16:28 -0300

    feat: add custom query methods to FuelPumpRepository and FuelTypeRepository

--- src/main/java/com/jessicavieiradev/postoGasolina/repository/FuelPumpRepository.java
+++ src/main/java/com/jessicavieiradev/postoGasolina/repository/FuelPumpRepository.java
@@ -1,9 +1,16 @@
 package com.jessicavieiradev.postoGasolina.repository;
 
 import com.jessicavieiradev.postoGasolina.domain.entity.FuelPump;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.UUID;
 
-public interface FuelPumpRepository extends CrudRepository<FuelPump, UUID> {
+public interface FuelPumpRepository extends JpaRepository<FuelPump, UUID> {
+    boolean existsByName(String name);
+
+    @Query(value = "SELECT * FROM fuel_pumps WHERE id = ?", nativeQuery = true)
+    Optional<FuelPump> findByIdIncludingInactive(@Param("id") UUID id);
 }
--- src/main/java/com/jessicavieiradev/postoGasolina/repository/FuelTypeRepository.java
+++ src/main/java/com/jessicavieiradev/postoGasolina/repository/FuelTypeRepository.java
@@ -1,12 +1,16 @@
 package com.jessicavieiradev.postoGasolina.repository;
 
 import com.jessicavieiradev.postoGasolina.domain.entity.FuelType;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface FuelTypeRepository extends JpaRepository<FuelType, UUID> {
     boolean existsByName(String name);
+
+    @Query(value = "SELECT * FROM fuel_types WHERE id = ?", nativeQuery = true)
+    Optional<FuelType> findByIdIncludingInactive(@Param("id") UUID id);
 }
