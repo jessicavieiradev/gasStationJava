4f90a055d8a864bfb11de083324a3617d103dfba
“jessicavieiradev” <misspandaicebear@gmail.com>
2026-05-18 09:14:12 -0300

    feat: add Fuel Supply DTOs

---
+++ src/main/java/com/jessicavieiradev/postoGasolina/dto/FuelSupplyDTO/FuelSupplyRequest.java
@@ -0,0 +1,14 @@
+package com.jessicavieiradev.postoGasolina.dto.FuelSupplyDTO;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record FuelSupplyRequest(
+        BigDecimal totalAmount,
+        BigDecimal literage,
+        @NotNull UUID fuelPumpId,
+        LocalDateTime date
+) {}
---
+++ src/main/java/com/jessicavieiradev/postoGasolina/dto/FuelSupplyDTO/FuelSupplyResponse.java
@@ -0,0 +1,18 @@
+package com.jessicavieiradev.postoGasolina.dto.FuelSupplyDTO;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record FuelSupplyResponse(
+        UUID id,
+        LocalDateTime date,
+        String fuelTypeName,
+        String fuelPumpName,
+        BigDecimal totalAmount,
+        BigDecimal literage,
+        UUID fuelPumpId,
+        String status,
+        LocalDateTime createdAt
+) {
+}
