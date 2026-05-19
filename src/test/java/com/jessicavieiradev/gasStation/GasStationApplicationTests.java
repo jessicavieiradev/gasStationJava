7aab9d48c529c8b10865c9a55cab86f321ee908a
“jessicavieiradev” <misspandaicebear@gmail.com>
2026-05-19 13:45:37 -0300

    fix: rename application test to match current name

--- src/test/java/com/jessicavieiradev/gasStation/GasStationApplicationTests.java
+++ src/test/java/com/jessicavieiradev/gasStation/GasStationApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PostoGasolinaApplicationTests {
+class GasStationApplicationTests {
 
 	@Test
 	void contextLoads() {
